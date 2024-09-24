02825d6fc36a3b93e815a34fe73992481e1c95df
Alibi <yoooholly71@gmail.com>
2024-09-24 18:24:23 +0200

    push

--- src/buildins/pwd.c
+++ src/buildins/pwd.c
@@ -6,8 +6,18 @@
 /*   By: ayermeko <ayermeko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 17:58:07 by ayermeko          #+#    #+#             */
-/*   Updated: 2024/09/24 18:02:23 by ayermeko         ###   ########.fr       */
+/*   Updated: 2024/09/24 18:24:13 by ayermeko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+
+int	pwd(void)
+{
+	char	cwd[PATH_MAX];
+
+	getcwd(cwd, PATH_MAX);
+	ft_putstr_fd(cwd, 1);
+	ft_putstr_fd("\n", 1);
+	return (0);
+}
